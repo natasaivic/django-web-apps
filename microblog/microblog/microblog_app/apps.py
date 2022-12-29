c8473ed1ba08c9d8f1c286854ad78a282137f1e7
Natasa Ivic <natasa.popovic.ivic@gmail.com>
2022-12-28 23:44:32 -0800

    Blog app

---
+++ microblog/microblog/microblog_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MicroblogAppConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "microblog_app"
---
+++ microblog/microblog/microblog_app/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.CharField(max_length=3000)
+    image = models.ImageField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    created_on.editable = True
+
+    def __str__(self):
+        return self.text
